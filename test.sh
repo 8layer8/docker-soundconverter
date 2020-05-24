e663a65818da566522601fd419a7646c9f09279e
8layer8 <brad@ntbrad.com>
2020-05-24 00:15:00 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+# Dockerfile
+# run it with:
+
+# source proxy.sh
+
+docker rm docker-soundconverter-test
+docker rmi docker-soundconverter
+docker build -t mindcrime30/docker-soundconverter:0.0.1 .
+
+# docker images
+docker run --rm -p 5805:5800 --name docker-soundconverter-test mindcrime30/docker-soundconverter:0.0.1
+
+# Put version above, and then:
+# docker login
+# yada yada
+# docker push mindcrime30/docker-soundconverter:0.0.1
+
+
